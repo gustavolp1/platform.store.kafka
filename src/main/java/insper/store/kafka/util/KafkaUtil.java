449064072503b8e628b98fda59d3d49bba17fab2
aguacrunch <pachecoglp02@gmail.com>
2024-05-26 22:38:49 -0300

    kafka

---
+++ src/main/java/insper/store/kafka/util/KafkaUtil.java
@@ -0,0 +1,23 @@
+package main.java.insper.store.kafka.util;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class KafkaUtil {
+
+    public static KafkaTemplate<String, String> createKafkaTemplate(String bootstrapServers) {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        
+        ProducerFactory<String, String> producerFactory = new DefaultKafkaProducerFactory<>(configProps);
+        return new KafkaTemplate<>(producerFactory);
+    }
+}
