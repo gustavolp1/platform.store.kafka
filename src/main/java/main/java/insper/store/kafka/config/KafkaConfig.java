3031eb21bd05e1b184cc83ae1f58bf7ee20a8bbe
gustavolp1 <gustavolp1@al.insper.edu.br>
2024-05-27 00:41:46 -0300

    kafka fix 2

--- src/main/java/main/java/insper/store/kafka/config/KafkaConfig.java
+++ src/main/java/main/java/insper/store/kafka/config/KafkaConfig.java
@@ -7,6 +7,7 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.*;
 
 import java.util.HashMap;
